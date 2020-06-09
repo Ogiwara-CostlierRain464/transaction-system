63734da6efb4b6490c2f73b2ffb11524066cb835
ogiwara <yushiogiwara@gmail.com>
2020-06-09 11:14:24 +0900

    add TXNExecutor

--- silo/txn_executor.h
+++ silo/txn_executor.h
@@ -1,12 +1,40 @@
 #ifndef TRANSACTIONSYSTEM_TXN_EXECUTOR_H
 #define TRANSACTIONSYSTEM_TXN_EXECUTOR_H
 
+#include <cstdint>
+#include <vector>
+#include "op_elemet.h"
+#include "tuple.h"
+#include "step.h"
+
+/**
+ * TXN Status.
+ * @see TXN System P128
+ */
+enum class TXNStatus: uint8_t {
+  Active, Committed, Aborted
+};
+
 /**
  * Transaction Executor
  * Transaction stands for TXN.
  */
 class TXNExecutor {
+public:
+  TXNExecutor(size_t threadId);
 
+  void abort();
+  void begin();
+  void displayWriteSet();
+  void lockWriteSet();
+  void read();
+
+private:
+  size_t threadId;
+  std::vector<ReadElement<Tuple>> readSet;
+  std::vector<WriteElement<Tuple>> writeSet;
+  std::vector<Step> steps;
+  TXNStatus status;
 };
 
 
