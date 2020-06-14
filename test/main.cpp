9c69e66424fa346d909aa98ac4f85fa34e2897dd
ogiwara <yushiogiwara@gmail.com>
2020-06-14 15:28:09 +0900

    add google test

---
+++ test/main.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+class Unit: public ::testing::Test{};
+
+TEST_F(Unit, init){
+EXPECT_EQ(1+1,2);
+}
+
+int main(int argc, char **argv){
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
