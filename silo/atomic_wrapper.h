23f224d6dc7c6a9d52211b6a440a2de7faac76dd
ogiwara <yushiogiwara@gmail.com>
2020-06-14 22:27:23 +0900

    add atomic_wrapper.h

---
+++ silo/atomic_wrapper.h
@@ -0,0 +1,40 @@
+#ifndef TRANSACTIONSYSTEM_ATOMIC_WRAPPER_H
+#define TRANSACTIONSYSTEM_ATOMIC_WRAPPER_H
+
+/**
+ * Wrappers of atomic built-in.
+ *
+ * @see https://gcc.gnu.org/onlinedocs/gcc/_005f_005fatomic-Builtins.html
+ * @see https://yohhoy.hatenablog.jp/entry/2014/12/21/171035
+ *
+ */
+
+template <typename T>
+T load(T &ptr){
+  return __atomic_load_n(&ptr, __ATOMIC_RELAXED);
+}
+
+template <typename T>
+T loadAcquire(T& ptr) {
+  return __atomic_load_n(&ptr, __ATOMIC_ACQUIRE);
+}
+
+template <typename T, typename T2>
+void store(T& ptr, T2 val) {
+  __atomic_store_n(&ptr, (T)val, __ATOMIC_RELAXED);
+}
+
+template <typename T, typename T2>
+void storeRelease(T& ptr, T2 val) {
+  __atomic_store_n(&ptr, (T)val, __ATOMIC_RELEASE);
+}
+
+template <typename T, typename T2>
+bool compareExchange(T& m, T& before, T2 after) {
+  return __atomic_compare_exchange_n(
+    &m, &before, (T)after, false,
+    __ATOMIC_ACQ_REL, __ATOMIC_ACQUIRE);
+}
+
+
+#endif //TRANSACTIONSYSTEM_ATOMIC_WRAPPER_H
--- silo/main.cpp
+++ silo/main.cpp
@@ -4,16 +4,22 @@
 #include <thread>
 #include "consts.h"
 
-void worker(int threadID){
+void worker(int threadID, const bool &start, const bool &quit){
 
 }
 
 int main(){
   alignas(CACHE_LINE_SIZE) bool start = false;
+  alignas(CACHE_LINE_SIZE) bool quit = false;
 
   std::vector<std::thread> threads;
+  threads.reserve(THREAD_NUM);
   for(int i = 0; i < THREAD_NUM; i++){
-    //threads.emplace_back(worker, i, )
+    threads.emplace_back(worker, i, start, quit);
+  }
+
+  for(auto &thread: threads){
+    thread.join();
   }
 
   return 0;
